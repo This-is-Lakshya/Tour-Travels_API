625b16025a97d02f1f9753263753645bd90fdcbd
Lakshya Singh <lakshyasinght2003@gmail.com>
2023-09-08 20:36:45 +0530

    n

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,5 @@
 import express from "express";
 import { deleteUser, getAllUser, getSingleUser, updateUser } from "../controllers/userController.js";
-import { verifyAdmin, verifyUser } from "../utils/verifyToken.js";
 const router = express.Router();
 
 // update new User
